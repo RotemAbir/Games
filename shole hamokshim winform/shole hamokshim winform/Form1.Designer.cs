128dd647121d13499346d00c2eac1688bac6b6c6
unknown <rotemabir25@gmail.com>
2021-02-15 15:42:16 +0200

    uploading games I created

---
+++ shole hamokshim winform/shole hamokshim winform/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace shole_hamokshim_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 261);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(278, 44);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(142, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Peep Solution";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 5;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 53);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 5;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(278, 205);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    }
+}
+
---
+++ shole hamokshim winform/shole hamokshim winform/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shole_hamokshim_winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            insert_labels();
+            choose_random_bombs(bombs_count);
+        }
+
+        int rows ;
+        int cols ;
+        int bombs_count = 5;
+        bool[,] bombs;
+        Label[,] labels;
+        
+        private void insert_labels()
+        {
+            rows = tableLayoutPanel3.RowCount;
+            cols=tableLayoutPanel3.ColumnCount;
+            tableLayoutPanel3.Controls.Clear();
+            labels = new Label[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Label l = new Label();
+                    l.Dock = DockStyle.Fill;
+                    l.BackColor = Color.LightGray;
+                    l.Click += L_Click;
+                    tableLayoutPanel3.SetRow(l, i);
+                    tableLayoutPanel3.SetColumn(l, j);
+                    tableLayoutPanel3.Controls.Add(l,j,i);
+                    labels[j, i] = l;
+                }
+            }
+        }
+
+        
+        private void choose_random_bombs(int count)
+        {
+            List<int> chooser = new List<int>();
+            for (int i = 0; i < rows*cols; i++)
+            {
+                chooser.Add(i);
+            }
+            bombs = new bool[rows, cols];
+            Random r = new Random();
+            while (count > 0)
+            {
+                int i = chooser[r.Next(chooser.Count)];
+                bombs[i % cols, i / cols] = true;
+                chooser.Remove(i);
+                count--;
+            }
+        }
+
+
+        private void change_bombs_color_to(Color br)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    if (bombs[x, y])
+                    {
+                        labels[x, y].BackColor = br;
+                    }
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    labels[x, y].BackColor = Color.LightGray;
+                    labels[x, y].Text = String.Empty;
+                }
+            }
+            choose_random_bombs(bombs_count);
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (bombs[i, j] == true)
+                    {
+                        labels[i, j].BackColor = Color.Yellow;
+                    }
+                }
+            }
+
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (bombs[i, j] == true)
+                    {
+                        labels[i, j].BackColor = Color.LightGray;
+                    }
+                }
+            }
+        }
+
+
+        int bombCounter;
+          private void L_Click(object sender, EventArgs e)
+          {
+              Label L = (Label)sender;
+            int row = tableLayoutPanel3.GetRow(L);
+            int col = tableLayoutPanel3.GetColumn(L);
+           
+            if (bombs[col,row] == true)
+                {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if(bombs[j,i]==true)
+                        {
+                            labels[j,i].BackColor = Color.Red;
+                        }
+                    }
+                }
+
+                MessageBox.Show("Game Over,you loose");
+            }
+
+          else
+                {
+                    L.BackColor = Color.White;
+                }
+
+            //בדיקה בצדדים
+          if(col !=tableLayoutPanel3.ColumnCount-1)
+            {
+                if(bombs[col+1,row]==true)
+                {
+                    bombCounter++;
+                }
+            }
+
+          if(col!=0)
+            {
+                if (bombs[col - 1, row] == true)
+                {
+                    bombCounter++;
+                }
+            }
+
+            //בדיקה למעלה ולמטה
+            if(row!=tableLayoutPanel3.RowCount-1)
+            {
+                if(bombs[col,row+1]==true)
+                {
+                    bombCounter++;
+                }
+            }
+
+            if(row!=0)
+            {
+                if(bombs[col,row-1]==true)
+                {
+                    bombCounter++;
+                }
+            }
+
+            if(bombCounter==0||L.BackColor==Color.Red)
+            {
+                L.Text = null;
+                bombCounter = 0;
+            }
+
+            else
+            {
+                L.Text = bombCounter.ToString();
+                bombCounter = 0;
+            }
+          }
+
+
+    }
+}
